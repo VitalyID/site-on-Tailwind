aff3f64b2a7f5ce6cc51675d93b4a39098c99969
Vitaly <s25698@ya.ru>
2025-06-05 03:08:58 +0500

    header is done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	mode: 'jit',
 	content: ['./public/**/*.html'],
 	theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+				'2xl': '1536px',
+				'3xl': '1920px',
+			},
+		},
 		extend: {
 			fontFamily: {
 				futura: ['"Futura PT"', 'sans-serif'],
